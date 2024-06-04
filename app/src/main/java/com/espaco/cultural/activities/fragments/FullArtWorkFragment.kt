2b6cc437c8270e52f26da0adc5ca0338997670e2
Fillipe143 <fillipedev.contato@gmail.com>
2024-06-03 23:29:40 -0300

    adicionado tratamento para nfcs invalidos

--- app/src/main/java/com/espaco/cultural/activities/fragments/FullArtWorkFragment.kt
+++ app/src/main/java/com/espaco/cultural/activities/fragments/FullArtWorkFragment.kt
@@ -8,6 +8,7 @@ import android.view.Display
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -49,6 +50,11 @@ class FullArtWorkFragment : Fragment() {
 
         ArtWorkDB.findArtWork(artWorkId) {
             if (it != null) binding.layout.visibility = View.GONE
+            else {
+                Toast.makeText(requireContext(), "Obra inexistente", Toast.LENGTH_SHORT).show()
+                requireActivity().onBackPressed()
+            }
+
             val artWork = it ?: ArtWork("None", "None", "None", "None", "")
             binding.textView.text = artWork.title
             binding.textView6.text = artWork.autor
